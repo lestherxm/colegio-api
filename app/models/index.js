31b06e6793c672f2481a5e202eff54ba7486682f
lestherxm <lestherxm@gmail.com>
2022-10-15 14:08:45 -0600

    index update

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,22 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, 
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    },
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
-});
+);
 
 const db = {};
 
